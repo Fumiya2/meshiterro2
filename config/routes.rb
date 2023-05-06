b804820e35e54c88367384f40ee82bb9a25de9b2
Fumiya2 <124821309+Fumiya2@users.noreply.github.com>
2023-05-06 14:13:03 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'meshiterros/new'
+  get 'meshiterros/index'
+  get 'meshiterros/show'
+  get 'meshiterros/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
